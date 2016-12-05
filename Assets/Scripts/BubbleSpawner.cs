c4a40543d8b8b80db91382231aba3678cd92a7e9
Connor Smith <c3smith@ucsd.edu>
2016-12-04 22:14:53 -0800

    Added bubbles and colliders to the room

---
+++ Assets/Scripts/BubbleSpawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleSpawner : MonoBehaviour {
+
+    public GameObject bubblePrefab;
+
+    public static BubbleSpawner instance;
+
+    void Awake() {
+
+        if (instance == null) {
+
+            instance = this;
+
+        }
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public GameObject makeBubble(string message, GameObject connectedObject) {
+        //Create the bubble above the object.
+        GameObject bubble = Instantiate(instance.bubblePrefab, connectedObject.transform.position, new Quaternion()) as GameObject;
+
+        bubble.GetComponent<TextBubbleScript>().fullMessage = message;
+
+        bubble.transform.SetParent(connectedObject.transform, true);
+        bubble.transform.localPosition = new Vector3(0, 0.2f, 0);
+        bubble.transform.localScale *= 0.25f;
+        bubble.transform.SetParent(null);
+
+        return bubble;
+    }
+}
